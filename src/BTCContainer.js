46626b1a80bd7737d1ceed442fe4c6072ff92900
subhankar <subhankardas695@gmail.com>
2023-06-16 11:33:12 +0530

    added logic to upload a dataset

--- src/BTCContainer.js
+++ src/BTCContainer.js
@@ -1,50 +1,68 @@
 import "./App.css";
+import { useState } from "react";
+import { Upload, Button } from "antd";
+import { UploadOutlined, LoadingOutlined } from "@ant-design/icons";
+import Papa from "papaparse";
 import RenderGraph from "./RenderGraph";
+
 function BTCContainer() {
-  const dataset = `Unix Timestamp,Date,Symbol,Open,High,Low,Close,Volume
-1.61888E+12,4/20/2021 0:02,BTCUSD,55717.47,55723,55541.69,55541.69,2.406733604
-1.61888E+12,4/20/2021 0:01,BTCUSD,55768.94,55849.82,55711.74,55717.47,0.5734577
-1.61888E+12,4/20/2021 0:00,BTCUSD,55691.79,55793.15,55691.79,55768.94,3.309903747
-1.61683E+12,3/27/2021 7:12,BTCUSD,55007,55030.9,55007,55028.07,0.09001
-1.61683E+12,3/27/2021 7:11,BTCUSD,55039.76,55039.76,54989.28,55007,7.70254363
-1.61683E+12,3/27/2021 7:10,BTCUSD,55037.8,55039.76,55037.8,55039.76,0.00090534
-1.61683E+12,3/27/2021 7:09,BTCUSD,55091.53,55091.53,55037.8,55037.8,0.523349722`;
+  // const [uploadedFile, setUploadedFile] = useState({});
+  const [csvData, setCSVData] = useState([]);
+  const [isParsingCSVFile, setParsingCSVFile] = useState(false);
 
-  // Parse the dataset
-  const rows = dataset.split("\n");
-  const headers = rows[0].split(",");
-  const data = rows.slice(1).map((row) => {
-    const values = row.split(",");
-    const rowData = {};
-    for (let i = 0; i < headers.length; i++) {
-      rowData[headers[i]] = values[i];
+  const parseCsv = (fileObj) => {
+    setParsingCSVFile(true);
+    Papa.parse(fileObj, {
+      skipEmptyLines: true,
+      header: true, //get array of objects
+      complete: (results) => {
+        setParsingCSVFile(false);
+        setCSVData(results.data);
+        console.log("post parse:", results.data);
+      },
+    });
+  };
+
+  const onUpload = ({ file, fileList }) => {
+    // setUploadedFile(file);
+    // if fileList contains a file, parse it
+    // in case file is removed, fileList is empty => donot parse
+    if (fileList.length > 0) {
+      parseCsv(file);
     }
-    return rowData;
-  });
-
-  // Extract month and count occurrences
-  const monthCount = {};
-  for (let i = 0; i < data.length; i++) {
-    const date = new Date(data[i].Date);
-    const month = date.toLocaleString("default", { month: "short" });
-    monthCount[month] = (monthCount[month] || 0) + 1;
-  }
-
-  // Prepare data for chart
-  const labels = Object.keys(monthCount);
-  const values = Object.values(monthCount);
-  console.log({ labels, values });
+  };
 
   return (
     <div className="parent-container">
       {/* parent is responsible for initialising data */}
       <section className="upload-container">
-        <button>Upload CSV</button>
+        <Upload
+          accept=".csv"
+          maxCount={1}
+          multiple={false}
+          beforeUpload={(file) => {
+            const reader = new FileReader();
+            reader.readAsText(file);
+            // Prevent upload
+            return false;
+          }}
+          onChange={onUpload}
+        >
+          <Button
+            className="upload-btn"
+            disabled={isParsingCSVFile}
+            icon={isParsingCSVFile ? <LoadingOutlined /> : <UploadOutlined />}
+          >
+            {isParsingCSVFile
+              ? `Parsing uploaded file`
+              : `Upload your data-set`}
+          </Button>
+        </Upload>
       </section>
 
       {/* pass initialised data to child */}
       <section className="child-container">
-        <RenderGraph />
+        <RenderGraph csvData={csvData} />
       </section>
     </div>
   );
