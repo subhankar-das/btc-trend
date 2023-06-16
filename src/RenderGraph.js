e175555605b6a3ccb9f188c52e73fb19ff315ab5
subhankar <subhankardas695@gmail.com>
2023-06-16 12:34:07 +0530

    added props, select to child

--- src/RenderGraph.js
+++ src/RenderGraph.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactECharts from "echarts-for-react";
+import { Select } from "antd";
 
-function RenderGraph() {
-  const options = {
-    grid: { top: 8, right: 8, bottom: 24, left: 36 },
+import PropTypes from "prop-types";
+function RenderGraph({ csvData, headers }) {
+  const nonIncludedHeaders = ["Unix Timestamp", "Date", "Symbol"];
+  const columnOptions =
+    headers.length > 0
+      ? headers
+          .filter((header) => !nonIncludedHeaders.includes(header))
+          .map((header) => {
+            const optionObj = {
+              value: header,
+              label: `# ${header}`,
+              disabled: headers.length === 0,
+            };
+            return optionObj;
+          })
+      : [];
+
+  const [columnValue, setColumnValue] = useState("# Open");
+  const onChangeColumn = (val) => {
+    setColumnValue(val);
+  };
+  const optionsForGraph = {
     xAxis: {
       type: "category",
       data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
@@ -13,28 +33,47 @@ function RenderGraph() {
     },
     series: [
       {
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        data: [150, 230, 224, 218, 135, 147, 260],
         type: "line",
-        smooth: true,
       },
     ],
-    tooltip: {
-      trigger: "axis",
-    },
   };
   return (
     <div className="visualization-container">
       <header className="header">Time Series Data Visualization</header>
       <section className="actions-container">
-        <button>Columns</button>
-        <button>Line</button>
-        <button>Bar</button>
+        <div className="choose-column">
+          <strong>Choose Column:</strong>
+          <Select
+            defaultValue="# Open"
+            value={columnValue}
+            style={{ width: 150 }}
+            disabled={csvData.length === 0}
+            onChange={onChangeColumn}
+            options={columnOptions}
+          />
+        </div>
+        <div className="choose-graph-type">
+          <strong>Choose Graph Type:</strong>
+          <button className="btn line-graph" disabled={headers.length === 0}>
+            Line
+          </button>
+          <button className="btn bar-graph" disabled={headers.length === 0}>
+            Bar
+          </button>
+        </div>
       </section>
+
       <section className="graph-container">
-        <ReactECharts option={options} />
+        <ReactECharts option={optionsForGraph} />
       </section>
     </div>
   );
 }
 
 export default RenderGraph;
+
+RenderGraph.propTypes = {
+  csvData: PropTypes.array.isRequired,
+  headers: PropTypes.array.isRequired,
+};
